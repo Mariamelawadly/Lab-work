13632e10524041d446750f2dd111fca5dcec4614
mariam elawadly <mariamelawadly@aucegypt.edu>
2021-06-08 14:11:35 +0200

    message

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "max.h"
+#include "KnapSack.h"
+int main()
+{
+int val[]={60,100,120};
+int wt[]={10,20,30};
+int W=50;
+int n= sizeof (val)/ size od (val[0]);
+cout<< KnapSack(W, wt, val, n);
+return 0;
+}
